93ebf7fa66265a9ce621fbbe9cbc547581cbaf03
ankittttss <ankitsaini955831@gmail.com>
2024-05-14 13:35:03 +0530

    Work:Day6

---
+++ Day6/AsyncandAwait.js
@@ -0,0 +1,26 @@
+
+// const p = new Promise((resolve,rejected)=>{
+//       resolve("Promise Reserved a value");
+// })
+
+// async function dosomething(){
+//     return p;
+// }
+
+// const dataPromise = dosomething();
+// dataPromise.then((res)=>console.log(res))
+
+//----------------------------------------------------
+// Let's try Async and Await//
+// Async and Await Combo is used to Handle the Promises//
+
+const p = new Promise((resolve,reject)=>{
+  resolve("Hi");
+})
+
+async function handle(){
+    const val = await p;
+    console.log(val);
+}
+
+handle();
