a7fdc32937df597b0ba1dd7268c8679e17dd65a4
ankittttss <ankitsaini955831@gmail.com>
2024-05-15 10:54:52 +0530

    Task:Day7 Json and It's Properties

---
+++ Day7/JSON.js
@@ -0,0 +1,10 @@
+// JSON -: Stands for Javacsript Object Notation//
+//Text Based data format following object syntax. 
+//Json exist as a string - usefull when you transmit data across a network//
+//Structure -: JSON is a string whose format very much resembles Javacsript Object Literal Format.
+//You can include data type such as string,arrays boolean//
+
+//Extrension for JSON file is -: .JSON
+
+// Data Respresentation Format//
+//Commonly used for API and Con
